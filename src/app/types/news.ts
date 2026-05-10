84315a0d191fae44e8ce99ca313da34df8c1bca6
Gabriella-Aquino <gabyyaquino8@gmail.com>
2026-05-10 11:54:48 -0300

    add tipes: INew, ICategory, IAuthor

---
+++ src/app/types/news.ts
@@ -0,0 +1,12 @@
+import { IAuthor } from './author';
+import { ICategory } from './category';
+
+export interface INews {
+  title: string;
+  subTitle: string;
+  content: string;
+  created: Date;
+  updated: Date;
+  author: IAuthor;
+  category: ICategory;
+}
