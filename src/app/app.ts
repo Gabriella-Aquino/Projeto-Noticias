6a3399a9ca7e5d78847fb653461e49be3df9fd69
Gabriella-Aquino <gabyyaquino8@gmail.com>
2026-05-10 11:53:33 -0300

    feat: Add static layout start

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzImageModule } from 'ng-zorro-antd/image';
+import { NewsCard } from "./components/news-card/news-card";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NzLayoutModule, NzImageModule, NewsCard],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
