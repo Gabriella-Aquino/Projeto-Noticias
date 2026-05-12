4f178d32fc9efe2a82cb2a916a9754788fc968e3
luizfernando110 <luizloona@gmail.com>
2026-05-12 14:23:15 -0300

    :sparkles: feat: Footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Footer } from './footer/footer';
 import { NavBar } from './nav-bar/nav-bar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavBar],
+  imports: [RouterOutlet, Footer, NavBar],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
