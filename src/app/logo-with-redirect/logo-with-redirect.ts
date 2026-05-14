186d3b5ad8b13af1e68b028fce6616b844037a95
Gabriella-Aquino <gabyyaquino8@gmail.com>
2026-05-13 21:16:54 -0300

    fix: undo changes in logoWithRedirect

--- src/app/logo-with-redirect/logo-with-redirect.ts
+++ src/app/logo-with-redirect/logo-with-redirect.ts
@@ -9,6 +9,5 @@ import { RouterLink } from '@angular/router';
 })
 export class LogoWithRedirect {
   showTitle = input(true, { transform: booleanAttribute });
-  title = input<string>()
   variant = input<'black' | 'white'>('black');
 }
