6daeb5e126d5812a17a8453f3f4bedf91675015a
Gabriella-Aquino <gabyyaquino8@gmail.com>
2026-07-04 19:29:58 -0300

    feat: adiciona create/update/delete em CategoryService e AuthorService
    Estende os serviços de categoria e autor com operações de escrita
    (POST/PATCH/DELETE) via Supabase REST, necessárias para o CRUD
    administrativo. Corrige também o spec de CategoryService, que
    importava uma classe inexistente.

--- src/app/services/author-service.spec.ts
+++ src/app/services/author-service.spec.ts
@@ -1,16 +1,56 @@
 import { TestBed } from '@angular/core/testing';
+import { provideHttpClient } from '@angular/common/http';
+import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
 
 import { AuthorService } from './author-service';
+import { environment } from '../../environments/environment.development';
 
 describe('AuthorService', () => {
   let service: AuthorService;
+  let httpMock: HttpTestingController;
+  const url = `${environment.supabaseUrl}author`;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [provideHttpClient(), provideHttpClientTesting()],
+    });
     service = TestBed.inject(AuthorService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should create an author', () => {
+    service.create({ name: 'Maria Silva' }).subscribe((author) => {
+      expect(author).toEqual({ id: 1, name: 'Maria Silva' });
+    });
+
+    const req = httpMock.expectOne(url);
+    expect(req.request.method).toBe('POST');
+    req.flush([{ id: 1, name: 'Maria Silva' }]);
+  });
+
+  it('should update an author', () => {
+    service.update(1, { name: 'Maria S. Souza' }).subscribe((author) => {
+      expect(author).toEqual({ id: 1, name: 'Maria S. Souza' });
+    });
+
+    const req = httpMock.expectOne(`${url}?id=eq.1`);
+    expect(req.request.method).toBe('PATCH');
+    req.flush([{ id: 1, name: 'Maria S. Souza' }]);
+  });
+
+  it('should delete an author', () => {
+    service.delete(1).subscribe();
+
+    const req = httpMock.expectOne(`${url}?id=eq.1`);
+    expect(req.request.method).toBe('DELETE');
+    req.flush(null);
   });
 });
--- src/app/services/author-service.ts
+++ src/app/services/author-service.ts
@@ -4,6 +4,8 @@ import { environment } from '../../environments/environment.development';
 import { map, Observable } from 'rxjs';
 import { IAuthor } from '../types/author';
 
+export type IAuthorCreate = Omit<IAuthor, 'id'>;
+
 @Injectable({
   providedIn: 'root',
 })
@@ -17,13 +19,31 @@ export class AuthorService {
     'Content-Type': 'application/json',
   });
 
+  private writeHeaders = this.headers.set('Prefer', 'return=representation');
+
   getAll(): Observable<IAuthor[]> {
     return this.http.get<IAuthor[]>(this.url, { headers: this.headers });
   }
 
   getAuthorById(id: number): Observable<IAuthor | null> {
-      return this.http
-        .get<IAuthor[]>(`${this.url}?id=eq.${id}`, { headers: this.headers })
-        .pipe(map((authors) => authors[0] ?? null));
-    }
+    return this.http
+      .get<IAuthor[]>(`${this.url}?id=eq.${id}`, { headers: this.headers })
+      .pipe(map((authors) => authors[0] ?? null));
+  }
+
+  create(author: IAuthorCreate): Observable<IAuthor> {
+    return this.http
+      .post<IAuthor[]>(this.url, author, { headers: this.writeHeaders })
+      .pipe(map((authors) => authors[0]));
+  }
+
+  update(id: number, author: IAuthorCreate): Observable<IAuthor> {
+    return this.http
+      .patch<IAuthor[]>(`${this.url}?id=eq.${id}`, author, { headers: this.writeHeaders })
+      .pipe(map((authors) => authors[0]));
+  }
+
+  delete(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.url}?id=eq.${id}`, { headers: this.headers });
+  }
 }
