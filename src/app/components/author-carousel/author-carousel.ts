de5c372ba4360aab0d604b486fe4a6aac85ccfd7
luizfernando110 <luizloona@gmail.com>
2026-07-16 09:38:17 -0300

    feature: Carousel responsiveness

--- src/app/components/author-carousel/author-carousel.ts
+++ src/app/components/author-carousel/author-carousel.ts
@@ -5,6 +5,8 @@ import { RouterLink } from '@angular/router';
 import { Image } from '../image/image';
 import { AuthorService } from '../../services/author-service';
 import { toSignal } from '@angular/core/rxjs-interop';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-author-carousel',
@@ -13,19 +15,48 @@ import { toSignal } from '@angular/core/rxjs-interop';
   styleUrl: './author-carousel.scss',
 })
 export class AuthorCarousel {
-  // originalList = AUTHORS_MOCK;
-
-  // groupedItems: IAuthor[][] = [];
-
   private authorService = inject(AuthorService);
+  private breakpointObserver = inject(BreakpointObserver);
 
   private authors = toSignal(this.authorService.getAll(), {
     initialValue: [] as IAuthor[],
   });
 
-  groupedItems = computed(() => this.chunkArray(this.authors(), 7));
+  private readonly customMediumQuery = '(max-width: 1200px)';
+
+  private itemsPerPage = toSignal(
+    this.breakpointObserver
+      .observe([
+        Breakpoints.Handset,       
+        Breakpoints.Tablet,        
+        this.customMediumQuery     
+      ])
+      .pipe(
+        map((result) => {
+          if (result.breakpoints[Breakpoints.HandsetPortrait] || result.breakpoints[Breakpoints.HandsetLandscape]) {
+            return 3;
+          }
+          if (result.breakpoints[Breakpoints.TabletPortrait] || result.breakpoints[Breakpoints.TabletLandscape]) {
+            return 5;
+          }
+          if (result.breakpoints[this.customMediumQuery]) {
+            return 6;
+          }
+          return 7;
+        })
+      ),
+    { initialValue: 7 }
+  );
+
+  groupedItems = computed(() => {
+    const size = this.itemsPerPage();
+    return this.chunkArray(this.authors(), size);
+  });
+
+  currentColumns = computed(() => this.itemsPerPage());
 
   chunkArray(array: IAuthor[], size: number) {
+    if (!array.length) return [];
     const result = [];
     for (let i = 0; i < array.length; i += size) {
       result.push(array.slice(i, i + size));
